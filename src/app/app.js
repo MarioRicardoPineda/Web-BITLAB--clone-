c3096b766df7f4098ef1ae1dfb2a5866005931eb
williamcruzme <sv.johncruz@gmail.com>
2019-06-28 19:14:27 -0600

    🔥 Unnecessary import images removed

--- src/app/app.js
+++ src/app/app.js
@@ -7,30 +7,3 @@ import "./js/menu"
 // Importamos los estiulos aquí
 
 import "./sass/style.scss"
-
-//  importar las imagenes
-
-// import img from "./assets/img"
-
-import "./assets/img/becas.png"
-import "./assets/img/favicon-bitlab.png"
-
-import "./assets/img/backend.jpg"
-import "./assets/img/carlos-muza.jpg"
-import "./assets/img/emprendimiento.jpg"
-import "./assets/img/estrategia-digital-elevada.jpg"
-import "./assets/img/evento.jpg"
-import "./assets/img/headway.jpg"
-import "./assets/img/hitesh-choudhary.jpg"
-import "./assets/img/img-evento-girls.jpg"
-import "./assets/img/intro-progra.jpg"
-import "./assets/img/intro-emprende.jpg"
-import "./assets/img/intro-strategy.jpg"
-import "./assets/img/product-school.jpg"
-import "./assets/img/ux-ui.jpg"
-
-import "./assets/img/logo-white.svg"
-import "./assets/img/academy.svg"
-import "./assets/img/Digital.svg"
-import "./assets/img/logo-fullcolor.svg"
-import "./assets/img/skills.svg"
